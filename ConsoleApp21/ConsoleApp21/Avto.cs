a7a1bcf50150ee6c044a8490357a7696844ddadf
ClyZeR1 <sergeistarodyb@gmail.com>
2025-02-20 21:23:10 +0300

    Add files via upload

---
+++ ConsoleApp21/ConsoleApp21/Avto.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp21
+{
+    class Avto
+    {
+        private string model;
+        private string color;
+        private int skor;
+
+        public string Model
+        {
+            get { return string.IsNullOrWhiteSpace(model) ? "неизвестный" : model; }
+            set { model = value; }
+        }
+
+        public string Color
+        {
+            get { return string.IsNullOrWhiteSpace(color) ? "неизвестный" : color; }
+            set { color = value; }
+        }
+
+        public int Skor
+        {
+            get { return skor; }
+            set
+            {
+                if (value < 60) skor = 60;
+                else if (value > 150) skor = 150;
+                else skor = value;
+            }
+        }
+
+        public Avto() { }
+
+        public Avto(string model, string color, int skor)
+        {
+            Model = model;
+            Color = color;
+            Skor = skor;
+        }
+
+        public void ShowInfo()
+        {
+            Console.WriteLine($"Модель: {Model}\tЦвет: {Color}\tСкорость: {Skor} км/ч");
+        }
+
+        public override string ToString()
+        {
+            return $"Модель: {Model}\tЦвет: {Color}\tСкорость: {Skor} км/ч";
+        }
+    }
+}
---
+++ ConsoleApp21/ConsoleApp21/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp21
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Avto avto = new Avto();
+            avto.Model = "Мерседес";
+            avto.Color = "Зеленый";
+            avto.Skor = 30;
+            avto.ShowInfo();
+
+            Console.WriteLine("Введите модель:");
+            string model = Console.ReadLine();
+            Console.WriteLine("Введите цвет:");
+            string color = Console.ReadLine();
+            Console.WriteLine("Введите скорость:");
+            int skor = int.Parse(Console.ReadLine());
+
+            Avto avto1 = new Avto(model, color, skor);
+            Console.WriteLine(avto1.ToString());
+
+            Console.ReadKey();
+        }
+    }
+}
