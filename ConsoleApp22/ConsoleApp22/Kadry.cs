e76b766ec89f2c0c2ec0bc569af0b1552761b2eb
ClyZeR1 <sergeistarodyb@gmail.com>
2025-02-20 22:07:15 +0300

    Add files via upload

---
+++ ConsoleApp22/ConsoleApp22/Kadry.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp22
+{
+    class Kadry
+    {
+    private string fam;
+    private int age;
+    private string dol;
+    private int staj;
+
+    public string Fam
+    {
+        get { return string.IsNullOrWhiteSpace(fam) ? "неизвестный" : fam; }
+        set { fam = value; }
+    }
+
+    public int Age
+    {
+        get { return age; }
+        set
+        {
+            if (value < 16) age = 16;
+            else if (value > 60) age = 60;
+            else age = value;
+        }
+    }
+
+    public string Dol
+    {
+        get { return string.IsNullOrWhiteSpace(dol) ? "неизвестная должность" : dol; }
+        set { dol = value; }
+    }
+
+    public int Staj
+    {
+        get { return staj; }
+        set
+        {
+            if (value < 0) staj = 0;
+            else if (value > 45) staj = 45;
+            else staj = value;
+        }
+    }
+
+    // Конструктор без параметров
+    public Kadry() { }
+
+    // Конструктор с параметрами
+    public Kadry(string fam, int age, string dol, int staj)
+    {
+        Fam = fam;
+        Age = age;
+        Dol = dol;
+        Staj = staj;
+    }
+
+    // Метод для вывода информации об объекте
+    public void ShowInfo()
+    {
+        Console.WriteLine($"Фамилия: {Fam}\tВозраст: {Age}\tДолжность: {Dol}\tСтаж: {Staj} лет");
+    }
+}
+}
---
+++ ConsoleApp22/ConsoleApp22/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp22
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Kadry kadry1 = new Kadry();
+            kadry1.Fam = "Иванов";
+            kadry1.Age = 25;
+            kadry1.Dol = "Инженер";
+            kadry1.Staj = 3;
+            kadry1.ShowInfo();
+
+            
+            Kadry kadry2 = new Kadry("Петров", 40, "Менеджер", 15);
+            kadry2.ShowInfo();
+
+            
+            Console.WriteLine("Фамилия :");
+            string fam = Console.ReadLine();
+            Console.WriteLine("Возраст :");
+            int age = int.Parse(Console.ReadLine());
+            Console.WriteLine("Должность :");
+            string dol = Console.ReadLine();
+            Console.WriteLine("Стаж :");
+            int staj = int.Parse(Console.ReadLine());
+
+            Kadry kadry3 = new Kadry(fam, age, dol, staj);
+            kadry3.ShowInfo();
+
+            Console.ReadKey();
+        }
+    }
+}
